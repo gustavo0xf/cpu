0ffabf3466b1c3391c548471ae30990af80e798a
lecaio08 <clmichalewicz@gmail.com>
2026-06-07 23:05:51 -0300

    Update control_unit.v

--- control_unit.v
+++ control_unit.v
@@ -1,14 +1,44 @@
 module control_unit (
-	input clk, rst
+
+    input clk,
+    input rst,
+
+    input [17:0] instruction,
+
+    output reg [2:0] opcode,
+
+    output reg [3:0] src1,
+    output reg [3:0] src2,
+    output reg [3:0] dst,
+
+    output reg we
+
 );
-	 // opcodes
-    parameter LOAD    = 3'b000;
-    parameter ADD     = 3'b001;
-    parameter ADDI    = 3'b010;
-    parameter SUB     = 3'b011;
-    parameter SUBI    = 3'b100;
-    parameter MUL     = 3'b101;
-    parameter CLEAR   = 3'b110;
-    parameter DISPLAY = 3'b111;
+
+    always @(*) begin
+
+        opcode = instruction[17:15];
+        dst    = instruction[14:11];
+        src1   = instruction[10:7];
+        src2   = instruction[3:0];
+
+        case(opcode)
+
+            3'b000: we = 1'b1; // LOAD
+            3'b001: we = 1'b1; // ADD
+            3'b010: we = 1'b1; // ADDI
+            3'b011: we = 1'b1; // SUB
+            3'b100: we = 1'b1; // SUBI
+            3'b101: we = 1'b1; // MUL
+
+            3'b110: we = 1'b0; // CLEAR
+
+            3'b111: we = 1'b0; // DISPLAY
+
+            default: we = 1'b0;
+
+        endcase
+
+    end
 
 endmodule
