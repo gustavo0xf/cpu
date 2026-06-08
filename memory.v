ecebd053bd586250a0f1d2cd6774590cb6d3eb69
lecaio08 <clmichalewicz@gmail.com>
2026-06-07 23:03:59 -0300

    Update memory.v

--- memory.v
+++ memory.v
@@ -2,10 +2,10 @@ module memory (
 	input 		[15:0] data,         // informação
 	input		[3:0]  src_addr1,    // endereço do registrador 1 (direto no FPGA)
 	input  	    [3:0]  src_addr2,    // endereço do registrador 2 (direto no FPGA)
-	input       [3:0]  dst_addr      // endereço do registrador de destino
+	input       [3:0]  dst_addr,      // endereço do registrador de destino
 	input              we, clk, rst, // sinais write enable, clock e reset
-	output reg  [15:0] rdata1,       // valor armazenado no registrador 1
-	output reg  [15:0] rdata2        // valor armazenado no registrador 2
+	output      [15:0] rdata1,       // valor armazenado no registrador 1
+	output      [15:0] rdata2        // valor armazenado no registrador 2
 );
 	// matriz p/ memoria e contador
 	reg [15:0] ram [15:0];
